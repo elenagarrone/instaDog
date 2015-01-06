54fb2140a248ace30117d1993eaae97960bb30bb
Elena <elena.15@hotmail.it>
2015-01-06 11:43:22 +0000

    problem with ajax for likes needs to be fixed!! Refactored code for post edit and new

--- spec/feature/like_feature_spec.rb
+++ spec/feature/like_feature_spec.rb
@@ -6,16 +6,32 @@ require_relative 'helpers/user_helper'
 feature 'like reviews' do
 
   before do
-    sign_up_first_user
-    create_post
+    Post.create(title: 'Smiley dog')
   end
 
-  scenario 'a user can like a review, which updates the review like count', js:true do
-    visit '/posts'
-    click_link 'Like'
-    expect(page).to have_content('1 Like')
-    click_link 'Like'
-    expect(page).to have_content('2 Likes')
+  scenario 'a user can like a review, which updates the review like count', js: true do
+    visit '/'
+    click_link 'Like Smiley dog'
+    expect(page).to have_content '1 Likes'
+    click_link 'Like Smiley dog'
+    expect(page).to have_content '2 Likes'
+  end
+
+  xscenario 'a user likes another post', js: true do
+    Post.create(title: 'Cute dog')
+    visit '/'
+    click_link 'Like Cute dog'
+    expect(page).to have_content '1 Likes'
+    expect(page).to have_content '0 Likes'
+  end
+
+  scenario 'a user likes another post AND UPDATE THE PAGE', js: true do
+    Post.create(title: 'Cute dog')
+    visit '/'
+    click_link 'Like Cute dog'
+    visit '/'
+    expect(page).to have_content '1 Likes'
+    expect(page).to have_content '0 Likes'
   end
 
 end
