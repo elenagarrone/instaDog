9116da974cad4ffe81ef05c81ff2cfd108c8270b
Elena <elena.15@hotmail.it>
2015-01-07 12:12:05 +0000

    fixed test for likes

--- spec/feature/like_feature_spec.rb
+++ spec/feature/like_feature_spec.rb
@@ -9,30 +9,12 @@ feature 'like reviews' do
     Post.create(title: 'Smiley dog')
   end
 
-  xscenario 'a user can like a review, which updates the review like count', js: true do
+  scenario 'a user can like a review, which updates the review like count', js: true do
     visit '/'
     click_on 'Like'
     expect(page).to have_content '1 Likes'
     click_on 'Like'
     expect(page).to have_content '2 Likes'
-  end
-
-  xscenario 'a user likes another post', js: true do
-    Post.create(title: 'Cute dog')
-    visit '/'
-    click_link 'Like'
-    expect(page).to have_content '1 Likes'
-    expect(page).to have_content '0 Likes'
-  end
-
-  xscenario 'a user likes another post AND UPDATE THE PAGE', js: true do
-    Post.create(title: 'Cute dog')
-    visit '/'
-    save_and_open_page
-    find("a#cutedog").click_link("Like")
-    visit '/'
-    expect(page).to have_content '1 Likes'
-    expect(page).to have_content '0 Likes'
   end
 
 end
